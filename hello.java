cd7df5c45480d318e1264808cbabebf4537353f9
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2023-09-10 10:43:50 +0530

    third commit

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ class hello{
     public static void main(String[] args) {
         System.out.println("hello git ");
         System.out.println("MITAOE git ");
+        System.out.println("hi ");
     }
 
 
